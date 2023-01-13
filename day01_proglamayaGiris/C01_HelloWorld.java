7b964302b1530a9a5287664d8e43c4fa5b24c628
MutluMesut <tankisimutlu@gmail.com>
2023-01-13 16:03:07 +0300

    13.01.2023

---
+++ day01_proglamayaGiris/C01_HelloWorld.java
@@ -0,0 +1,14 @@
+package day01_proglamayaGiris;
+
+public class C01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.print( "Bugün kodlamada ilk gunumuz");
+        System.out.println("Bu da ucuncu satir olsun");
+
+        //println = alt satira gecer
+        // print = bir sonra yazdirdigimiz ayni satirdan devam eder.
+
+
+    }
+}
---
+++ day01_proglamayaGiris/C02_Variables.java
@@ -0,0 +1,19 @@
+package day01_proglamayaGiris;
+
+public class C02_Variables {
+    public static void main(String[] args) {
+        int sayi = 20 ;
+        System.out.println(sayi);
+
+        System.out.println("sayi");
+
+        sayi =30;
+        System.out.println(sayi);
+
+        sayi= sayi+20;
+
+        System.out.println(sayi);//50
+
+    }
+
+}
